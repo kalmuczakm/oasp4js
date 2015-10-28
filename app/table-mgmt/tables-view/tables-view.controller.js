940d8a487bd96759e61f55472cfe2835b8eeef74
Mariusz <kalmuczakm@gmail.com>
2015-10-28 14:47:58 +0100

    removed unused code and changed pagination request

--- app/table-mgmt/tables-view/tables-view.controller.js
+++ app/table-mgmt/tables-view/tables-view.controller.js
@@ -1,9 +1,6 @@
 angular.module('app.table-mgmt')
     .controller('TablesViewCntl', function ($scope, tables, paginatedTableList, $state, globalSpinner) {
         'use strict';
-        var selectedTable = function () {
-            return $scope.selectedItems && $scope.selectedItems.length ? $scope.selectedItems[0] : undefined;
-        };
 
         $scope.openEditDialog = function (table) {
             $state.go('tableMgmt.details', {tableId: table.id});
@@ -24,28 +21,9 @@ angular.module('app.table-mgmt')
                 return paginatedTables;
             }).then(function (res) {
                 paginatedTableList = res;
-
-                //paginatedTableList.result = filterTables(res.result);
                 $scope.gridOptions.data = paginatedTableList.result;
             });
         };
-
-        /*function filterTables(tables) {
-            var filteredTables = [];
-            angular.forEach(tables, function(table) {
-                $scope.statusFilter(table) ? filteredTables.push(table) : '';
-            });
-            return filteredTables;
-        }*/
-
-        function multiplyPaginatedTableBy (table, times) {
-            var multipliedTable = table;
-            for(var i=1; i<times; i++) {
-                multipliedTable.result = table.result.concat(angular.copy(table.result));
-            }
-            multipliedTable.pagination.total *= times;
-            return multipliedTable;
-        }
 
         $scope.tableStatus = {
             free: true,
@@ -55,7 +33,7 @@ angular.module('app.table-mgmt')
 
         $scope.toggleFilter = function (kind) {
             if(kind === 'all') {
-                angular.forEach($scope.tableStatus, function(bool, attr) {
+                angular.forEach($scope.tableStatus, function(val, attr) {
                     $scope.tableStatus[attr] = true;
                 })
             } else {
@@ -89,62 +67,4 @@ angular.module('app.table-mgmt')
                 return tables.occupy(table).then($scope.reloadTables);
             });
         };
-
-        $scope.buttonDefs = [
-            {
-                label: 'TABLE_MGMT.EDIT',
-                onClick: function () {
-                    $scope.openEditDialog(selectedTable());
-                },
-                isActive: function () {
-                    return selectedTable();
-                }
-            },
-            {
-                label: 'TABLE_MGMT.RESERVE',
-                onClick: function () {
-                    globalSpinner.decorateCallOfFunctionReturningPromise(function () {
-                        return tables.reserve(selectedTable()).then($scope.reloadTables);
-                    });
-                },
-                isActive: function () {
-                    return selectedTable() && selectedTable().state === 'FREE';
-                }
-            },
-            {
-                label: 'TABLE_MGMT.CANCEL_RESERVATION',
-                onClick: function () {
-                    globalSpinner.decorateCallOfFunctionReturningPromise(function () {
-                        return tables.cancelReservation(selectedTable()).then($scope.reloadTables);
-                    });
-                },
-                isActive: function () {
-                    return selectedTable() && selectedTable().state === 'RESERVED';
-                }
-            },
-            {
-                label: 'TABLE_MGMT.OCCUPY',
-                onClick: function () {
-                    globalSpinner.decorateCallOfFunctionReturningPromise(function () {
-                        return tables.occupy(selectedTable()).then($scope.reloadTables);
-                    });
-                },
-                isActive: function () {
-                    return selectedTable() && (selectedTable().state === 'RESERVED' || selectedTable().state === 'FREE');
-                }
-            },
-            {
-                label: 'TABLE_MGMT.FREE',
-                onClick: function () {
-                    globalSpinner.decorateCallOfFunctionReturningPromise(function () {
-                        return tables.free(selectedTable()).then($scope.reloadTables);
-                    });
-                },
-                isActive: function () {
-                    return selectedTable() && selectedTable().state === 'OCCUPIED';
-                }
-            }
-        ];
-
-
     });
